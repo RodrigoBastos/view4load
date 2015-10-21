52c6faaddc92ca93eece8a1ec7b90e70007a6057
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2015-10-21 10:19:13 -0300

    Add templateColor attribute the load directive

--- dist/view4load.js
+++ dist/view4load.js
@@ -16,15 +16,11 @@ app.directive('load', function ($http)
     restrict: 'A',
     template: function (element, attrs){
 
-      console.log(attrs);
-
       var color = colors[attrs.templateColor];
 
-      var template = '<div ng-if="isShow" style="left: 0;top: 0;margin: 0;font-family: arial, sans-serif;font-weight: bold;visibility: hidden;z-index: 1050;position: absolute;text-align: center;width: 100%;height: 100%;">'+
-        '<div style="position: relative;display: inline-block;visibility: visible;font-size: 80%;padding: 6px 10px;'+color+'box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">'+
+      return '<div ng-if="isShow" style="left: 0;top: 0;margin: 0;font-family: arial, sans-serif;font-weight: bold;visibility: hidden;z-index: 1050;position: absolute;text-align: center;width: 100%;height: 100%;">' +
+        '<div style="position: relative;display: inline-block;visibility: visible;font-size: 80%;padding: 6px 10px;' + color + 'box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">' +
         'Carregando...</div></div>';
-
-      return template;//templates[attrs.view];
     },
     link: function (scope, element, attrs)
     {

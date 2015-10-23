b2b5dc0aec17e9f04da73c947a7609d74abbc1f6
Samuel das Chagas <samueldaschagas@gmail.com>
2015-10-23 14:35:23 -0300

    Fixed conflicts

--- source/js/partials/utils.js
+++ source/js/partials/utils.js
@@ -11,13 +11,15 @@
 var colors =  {
   default: 'background-color: #f9edbe;border: 1px solid #f0c36d;',
   primary: 'background-color: #62a4ff;border: 1px solid #78e5f0;',
-  second: 'background-color: #ffe8fe;border: 1px solid #dad6db;'
+  second: 'background-color: #ffe8fe;border: 1px solid #dad6db;',
+  dark: 'background-color: #000; border: 1px solid #ccc; color: #fff;'
 };
 
 var positions = {
   right: 'text-align: right',
   center: 'text-align: center',
-  left: 'text-align: left'
+  left: 'text-align: left',
+  bottom: 'bottom: 0'
 };
 
 var defaultValues = {

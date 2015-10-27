bff836f64524d9ac699f4b3ea83c5a91da6e0625
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2015-10-27 11:08:43 -0300

    New organized for templates on directive

--- source/js/partials/load.js
+++ source/js/partials/load.js
@@ -8,15 +8,30 @@ app.directive('load', function ($http)
     restrict: 'A',
     template: function (element, attrs){
 
-      var color =  attrs.template ? colors[attrs.template] : colors[defaultValues.color];
-      var position = attrs.templatePosition? positions[attrs.templatePosition] : positions[defaultValues.position];
-      var size = attrs.templateSize ? sizes[attrs.templateSize] : sizes[defaultValues.size];
-      var label = attrs.templateLabel ? attrs.templateLabel : defaultValues.label;
+      var activeTemplate = attrs.template ?  templates[attrs.template] : templates.template1;
 
+      //INTERN
+      var color  = attrs.templateColor ? colors[attrs.templateColor] : activeTemplate.intern.color;
 
-      return '<div ng-if="isShow" style="left: 0;top: 0;margin: 0;font-family: arial, sans-serif;font-weight: bold;visibility: hidden;z-index: 1050;position: absolute;'+position+';width: 100%;height: 100%;">' +
-        '<div style="position: relative;display: inline-block;visibility: visible;font-size: 80%;' + size + color + 'box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">' +
-        ''+label+'</div></div>';
+      var size = attrs.templateSize ? sizes[attrs.templateSize] : activeTemplate.intern.size;
+      var label = attrs.templateLabel ? attrs.templateLabel : activeTemplate.intern.label;
+      var intern = activeTemplate.intern.general;
+
+      //EXTERN
+      var position = attrs.templatePosition ? positions[attrs.templatePosition] : activeTemplate.extern.position;
+      var font = attrs.templateFont ? fonts[attrs.templateFont] : activeTemplate.extern.font;
+      var extern = activeTemplate.extern.general;
+
+      return '<div ng-if="isShow" style="'+ extern +';'+ position + '"><div style="'+ font +';'+ intern + ';'+color+';'+size+'">'+label+'</div></div>';
+
+      //var color =  attrs.template ? colors[attrs.template] : colors[defaultValues.color];
+      //var position = attrs.templatePosition? positions[attrs.templatePosition] : positions[defaultValues.position];
+      //var size = attrs.templateSize ? sizes[attrs.templateSize] : sizes[defaultValues.size];
+      //var label = attrs.templateLabel ? attrs.templateLabel : defaultValues.label;
+      //
+      //return '<div ng-if="isShow" style="left: 0;top: 0;margin: 0;font-family: arial, sans-serif;font-weight: bold;visibility: hidden;z-index: 1050;position: absolute;'+position+';width: 100%;height: 100%;">' +
+      //  '<div style="position: relative;display: inline-block;visibility: visible;font-size: 80%;' + size + color + 'box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">' +
+      //  ''+label+'</div></div>';
     },
     link: function (scope, element, attrs)
     {
--- source/js/partials/utils.js
+++ source/js/partials/utils.js
@@ -8,6 +8,51 @@
  * primary: Blue Color
  * second: White Color
  */
+
+
+
+//'<div ng-if="isShow" style="left: 0;top: 0;margin: 0;font-family: arial, sans-serif;font-weight: bold;visibility: hidden;z-index: 1050;position: absolute;'+position+';width: 100%;height: 100%;">' +
+////  '<div style="position: relative;display: inline-block;visibility: visible;font-size: 80%;' + size + color + 'box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">' +
+////  ''+label+'</div></div>'
+
+
+
+var internDefault = {
+  general:'position: relative;display: inline-block;visibility: visible;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2)',
+  color: 'background-color: #f9edbe;border: 1px solid #f0c36d',
+  size: 'font-size: 80%',
+  label: 'Loading...'
+};
+
+var externDefault = {
+  general: 'margin: 0;visibility: hidden;z-index: 1050;width: 100%;height: 100%',
+  position: 'left: 0;top: 0;position: absolute;text-align: center',
+  font: 'font-family: arial, sans-serif;font-weight: bold'
+};
+
+var templates = {
+  template1: {
+    intern: internDefault,
+    extern: externDefault
+  },
+  template2: {
+    intern: {
+      general:'position: relative;display: inline-block;visibility: visible;box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2)',
+      color: 'background-color: #f9edbe;border: 1px solid #f0c36d',
+      font: 'font-family: arial, sans-serif;font-weight: bold',
+      size: 'font-size: 80%',
+      label: 'Loading...'
+    },
+    extern: {
+      general: 'margin: 0;visibility: hidden;z-index: 1050;width: 100%;height: 100%',
+      position: 'left: 0;top: 0;position: absolute;text-align: center'
+    }
+  }
+
+
+};
+
+
 var colors =  {
   default: 'background-color: #f9edbe;border: 1px solid #f0c36d;',
   primary: 'background-color: #62a4ff;border: 1px solid #78e5f0;',
@@ -26,9 +71,12 @@ var sizes = {
 	lg: 'padding: 20px 50px; font-size:25px;'
 };
 
+var fonts = {
+
+};
+
 var defaultValues = {
   color: 'default',
   position: 'center',
-  label: 'LOADING..',
-  size: 'lg'
+  label: 'LOADING..'
 };
